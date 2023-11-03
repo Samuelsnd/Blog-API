86739baf22624c80133f5f7974434376eaaee7e9
Samuelsnd <samueldasilva9211@gmail.com>
2023-11-02 23:24:24 -0300

    creating the form the project

--- src/main/java/com/spring/CodeBlog/controller/PostController.java
+++ src/main/java/com/spring/CodeBlog/controller/PostController.java
@@ -2,12 +2,17 @@ package com.spring.CodeBlog.controller;
 
 import com.spring.CodeBlog.model.Post;
 import com.spring.CodeBlog.service.PostService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -32,4 +37,20 @@ public class PostController {
         mv.addObject("post", post);
         return mv;
     }
+
+    @GetMapping(value = "/newpost")
+    public String getPostForm() {
+        return "postForm";
+    }
+
+    @PostMapping("/newpost")
+    public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes) {
+        if(result.hasErrors()) {
+            return "redirect:/newpost";
+        }
+        post.setData(LocalDate.now());
+        postService.save(post);
+        return "redirect:/posts";
+    }
+
 }
